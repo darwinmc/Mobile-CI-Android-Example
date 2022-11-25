d52be14681ef92f7f4d03e7d88a5b5647198e2c5
darwinmc <manueldarwin@gmail.com>
2022-11-25 18:26:11 +0530

    Emulator workaround

--- emulator.sh
+++ emulator.sh
@@ -7,8 +7,8 @@ sdkmanager "system-images;android-23;default;armeabi-v7a"
 # Create an ARM AVD emulator, with a 100 MB SD card storage space. Echo "no"
 # because it will ask if you want to use a custom hardware profile, and you don't.
 # https://medium.com/@AndreSand/android-emulator-on-docker-container-f20c49b129ef
-echo no | avdmanager create avd --force --sdcard 100M --name test -k "system-images;android-23;default;armeabi-v7a"
-$ANDROID_HOME/emulator/emulator -avd test -no-audio -no-window &
+echo no | avdmanager create avd --force --sdcard 100M --name test -k "system-images;android-19;default;armeabi-v7a"
+$ANDROID_HOME/emulator/emulator -avd test -no-window &
 
 ## Wait for Android to finish booting
 wget --quiet --output-document=android-wait-for-emulator https://raw.githubusercontent.com/travis-ci/travis-cookbooks/master/community-cookbooks/android-sdk/files/default/android-wait-for-emulator
